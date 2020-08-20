d15ee581e776ce6a5a7af168438e72f637b8e1e3
Yaswanth <65965757+Yaswanth-L@users.noreply.github.com>
2020-08-20 20:00:13 +0530

    Update ssem6.js

--- src/screens/ssem/ssem6.js
+++ src/screens/ssem/ssem6.js
@@ -97,7 +97,7 @@ const Ssem6=()=>{
         }
         else
         M.toast({html:'Sorry , Invalid Input',classes:"#ff1744 red accent-3"})
-        setTimeout(()=>window.location.reload(),3000);
+        setTimeout(()=>window.location.replace("https://calculator-gpa.netlify.app/"),3000);
     }
     if (data=="CSE")
     {
