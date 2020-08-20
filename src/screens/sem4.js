ae9f6b45fa1be933fb9f7f24f2c626f201ee1974
Yaswanth <65965757+Yaswanth-L@users.noreply.github.com>
2020-08-20 19:53:12 +0530

    Update sem4.js

--- src/screens/sem4.js
+++ src/screens/sem4.js
@@ -46,7 +46,7 @@ const Sem4 = () => {
         }
         else
         M.toast({html:'Sorry , Invalid Input',classes:"#ff1744 red accent-3"})
-        setTimeout(()=>window.location.reload(),3000);
+        setTimeout(()=>window.location.replace("https://calculator-gpa.netlify.app/"),3000);
     }
     return (
         <div className="cls1 input-field">
